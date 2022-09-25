be6a45439fb2259303f7fb835e5016707c1b7023
Anirudh Acharya <acharyaani92@gmail.com>
2022-09-25 05:44:56 +0530

    Add override for database if provided by environment
    From: https://github.com/heroku/python-getting-started/blob/main/gettingstarted/settings.py

--- ical/settings.py
+++ ical/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import dj_database_url
 import os
 
 from django.test.runner import DiscoverRunner
@@ -91,6 +92,14 @@ DATABASES = {
     }
 }
 
+if "DATABASE_URL" in os.environ:
+    # Configure Django for DATABASE_URL environment variable.
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=MAX_CONN_AGE, ssl_require=True)
+
+    # Enable test database if found in CI environment.
+    if "CI" in os.environ:
+        DATABASES["default"]["TEST"] = DATABASES["default"]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
